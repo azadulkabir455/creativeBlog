6e7b24fac58429d29ae46382472edafdd3a5da03
azad ul kabir <azadulkabir10@gmail.com>
2022-10-19 21:46:10 +0600

    Global Header done with theming color:)

--- src/GlobalWidgets/GlobalHeader/GlobalHeader.jsx
+++ src/GlobalWidgets/GlobalHeader/GlobalHeader.jsx
@@ -1,36 +1,35 @@
-import React,{useContext} from 'react'
-import {Link} from "react-router-dom"
+import React, { useContext } from 'react'
+import { Link } from "react-router-dom"
 import "../../Resources/Css/GlobalWidgets/globalHeader.scss";
-import {GlobalContextProvider} from "../../ContextApi/GlobalContext"
+import { GlobalContextProvider } from "../../ContextApi/GlobalContext"
+import { BsArrowRightShort } from "react-icons/bs";
 
 export default function GlobalHeader() {
-    const {theme,toggleTheme} = useContext(GlobalContextProvider)
+    const { theme, toggleTheme } = useContext(GlobalContextProvider)
     return (
         <>
-            <nav className="navbar navbar-expand-lg" id={theme}>
+            <nav className=" myNavbar navbar navbar-expand-lg" id={theme}>
                 <div className="container">
-                    <a className="navbar-brand" >Navbar</a>
+                    <Link className="navbar-brand" to="/">Akash</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                               <Link to="/" className='nav-link'>Home</Link>
+                                <Link to="/" className='nav-link'>Home</Link>
                             </li>
                             <li className="nav-item">
-                               <Link to="/contact" className='nav-link'>Contact</Link>
+                                <Link to="/contact" className='nav-link'>Contact</Link>
                             </li>
                             <li className="nav-item">
-                                <button>Login</button>
-                            </li>
-                            <li className="nav-item">
-                                <button onClick={toggleTheme}>dark moode</button>
+                                <button className="menuButton">Login <BsArrowRightShort /></button>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
+            <button onClick={toggleTheme}>theme</button>
         </>
     )
 }
