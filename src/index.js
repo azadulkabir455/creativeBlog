3af848983c00f7ea6160d65582adaa2a9178c26b
azad ul kabir <azadulkabir10@gmail.com>
2022-11-04 20:51:55 +0600

    firebase deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalContextContainer } from "./ContextApi/GlobalContext"
 import App from './App';
@@ -17,3 +18,4 @@ root.render(
 
   </React.StrictMode>
 );
+reportWebVitals();
